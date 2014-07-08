d060bbf9ef82904586c64a2eed6a9cfe9de54ccb
Massimo Cardellicchio <massimo@massimo-Satellite-A100.(none)>
2014-07-08 18:31:45 +0200

    parametri teleop

--- saetta_base/src/saetta_teleop.cpp
+++ saetta_base/src/saetta_teleop.cpp
@@ -10,31 +10,19 @@ class TeleopSaetta
 {
 public:
   TeleopSaetta();
-
-
-private:
-
-  
-  ros::NodeHandle nh_;
   int linear_, angular_;
   double l_scale_, a_scale_;
+
+private:
+  ros::NodeHandle nh_;
   ros::Publisher vel_pub_;
   ros::Subscriber joy_sub_;
 
   void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 };
 
-TeleopSaetta::TeleopSaetta():
-  linear_(1),
-  angular_(2),
-  l_scale_(2.0),
-  a_scale_(2.0)
+TeleopSaetta::TeleopSaetta()
 {
-  nh_.param("axis_linear", linear_, linear_);
-  nh_.param("axis_angular", angular_, angular_);
-  nh_.param("scale_angular", a_scale_, a_scale_);
-  nh_.param("scale_linear", l_scale_, l_scale_);
-
    joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopSaetta::joyCallback, this);
    vel_pub_ =  nh_.advertise<geometry_msgs::Twist>("/saetta/velocity",1);  
 }
@@ -52,9 +40,12 @@ void TeleopSaetta::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "teleop_saetta");
-  TeleopSaetta teleop_saetta;
-  ros::NodeHandle n;
-
+  TeleopSaetta* teleop_saetta = new TeleopSaetta;
+  ros::NodeHandle n("~");
+  n.param<int>("axis_linear", teleop_saetta->linear_, 1);
+  n.param<int>("axis_angular", teleop_saetta->angular_, 0);
+  n.param<double>("scale_angular", teleop_saetta->a_scale_, 0.5);
+  n.param<double>("scale_linear", teleop_saetta->l_scale_, 0.1);
   int rate = 4;
   ros::Rate r(rate);
   while (n.ok())
